a7590ae60885bf41c27b67c311c85f5427ff25d7
Aly Ragab <alyragab70@gmail.com>
2020-10-16 05:13:36 +0200

    adding main

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module LambdaFunc-rds-alert
 
 go 1.15
 
-require github.com/aws/aws-lambda-go v1.19.1 // indirect
+require github.com/aws/aws-lambda-go v1.19.1
---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Request struct {
+	Records []struct {
+		SNS struct {
+			Type       string `json:"Type"`
+			Timestamp  string `json:"Timestamp"`
+			SNSMessage string `json:"Message"`
+		} `json:"Sns"`
+	} `json:"Records"`
+}
+
+type SNSMessage struct {
+	AlarmName      string `json:"AlarmName"`
+	NewStateValue  string `json:"NewStateValue"`
+	NewStateReason string `json:"NewStateReason"`
+}
+
+type SlackMessage struct {
+	Text        string       `json:"text"`
+	Attachments []Attachment `json:"attachments"`
+}
+
+type Attachment struct {
+	Text  string `json:"text"`
+	Color string `json:"color"`
+	Title string `json:"title"`
+}
+
+// 1.Parsing the SNS data based on the AlarmName.
+// 2.Sending it as argument to the slack function "buildMessage".
+func handler(request Request) error {
+	var snsMessage SNSMessage
+	err := json.Unmarshal([]byte(request.Records[0].SNS.SNSMessage), &snsMessage)
+	if err != nil {
+		return err
+	}
+
+	// Getting the Alarm Name
+	log.Printf("New alarm: %s - Reason: %s", snsMessage.AlarmName, snsMessage.NewStateReason)
+	slackMessage := buildMessage(snsMessage)
+	sendToSlack(slackMessage)
+	log.Println("Notification has been sent")
+	return nil
+}
+
+// Building/Formating Slack Message from SlackMessage Struct
+func buildMessage(message SNSMessage) SlackMessage {
+	return SlackMessage{
+		Text: fmt.Sprintf("%s", message.AlarmName),
+		Attachments: []Attachment{
+			Attachment{
+				Text:  message.NewStateReason,
+				Color: "danger",
+				Title: "Reason",
+			},
+		},
+	}
+}
+
+func sendToSlack(message SlackMessage) error {
+	client := &http.Client{}
+	data, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", os.Getenv("SLACK_WEBHOOK"), bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		fmt.Println(resp.StatusCode)
+		return err
+	}
+
+	return nil
+}
+func main() {
+	lambda.Start(handler)
+}
